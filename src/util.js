a7780d2bc604eeee4f257b7cf325641995baa6f8
Sean Wu <sean840411@gmail.com>
2021-03-16 11:48:01 +0800

    fix utils

--- src/util.js
+++ src/util.js
@@ -1,11 +1,12 @@
 const utils = {
 
-    parseMsg: (payload) => {
-        function toDateTime(secs) {
+    parseMsg: (secs) => {
         var t = new Date(1970, 0, 1); // Epoch
         t.setSeconds(secs);
         return t;
-        }
+    },
+    calDistance: (long1, lat1, long2, lat2) => {
+        // TODO: return the distance of two ppl
     }
 }
 export default utils;
