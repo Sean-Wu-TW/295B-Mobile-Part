c7045680eb89b8393ac6da198955dfaf93d275ca
KaiOnEarth <hzkonearth@gmail.com>
2021-04-04 02:53:20 -0700

    fix bugs

--- src/component/StackNavigator.js
+++ src/component/StackNavigator.js
@@ -6,33 +6,6 @@ import NearbyList from './NearbyList';
 
 const Stack = createStackNavigator();
 
-
-// const StackNavigator = ({auth, setAuth}) => {
-//     return (
-//       <Stack.Navigator initialRouteName="Dash">
-//         <Stack.Screen name="AddFriend">
-//           {(props) => <AddFriend {...props} auth={auth} />}
-//         </Stack.Screen>
-//         <Stack.Screen name="Dash">
-//           {(props) => (
-//             <>
-//               <MessagesScreen {...props} auth={auth} />
-//               <Dash {...props} setAuth={setAuth} auth={auth} />
-//             </>
-//           )}
-//         </Stack.Screen>
-//         <Stack.Screen
-//           name="Example"
-//           component={Example}
-//           options={({route}) => ({
-//             title: route.params.userName,
-//             headerBackTitleVisible: false,
-//           })}></Stack.Screen>
-//         <Stack.Screen name="Nearby List" component={NearbyList}></Stack.Screen>
-//       </Stack.Navigator>
-//     );
-// }
-
 const StackNavigator2 = ({auth}) => {
     console.log(auth);
     return (
@@ -65,51 +38,5 @@ const NearbyStackNavigator = ({auth}) => {
         </Stack.Navigator>
       );
 }
-
-
-
-
-
-
-// import React from "react";
-// import { createStackNavigator } from "@react-navigation/stack";
-
-// import Home from "./home";
-// import About from "./about";
-
-// const Stack = createStackNavigator();
-
-// const StackNavigator2 = () => {
-//   return (
-//     <Stack.Navigator initialRouteName="Dash">
-//       <Stack.Screen  name="MessagesScreen" component={Home} option={
-//           {title: 'Home'}}/>
-//       <Stack.Screen
-//         name="Example"
-//         component={Example} />
-//     </Stack.Navigator>
-//   );
-// }
-
-// export { StackNavigator2 };
-
-
-// import React from "react";
-// import { createStackNavigator } from "@react-navigation/stack";
-
-// import Home from "./home";
-// import About from "./about";
-
-// const Stack = createStackNavigator();
-
-// const StackNavigator2 = () => {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen name="Home" component={Home} option={
-//           {title: 'Home'}}/>
-//       <Stack.Screen name="About" component={About} />
-//     </Stack.Navigator>
-//   );
-// }
 
 export { StackNavigator2, NearbyStackNavigator };
