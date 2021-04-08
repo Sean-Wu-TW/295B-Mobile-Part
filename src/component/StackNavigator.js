a76dbe49cbf67f22d2762ab836bda8c1359bc55d
KaiOnEarth <hzkonearth@gmail.com>
2021-04-07 17:03:21 -0700

    fix bugs

--- src/component/StackNavigator.js
+++ src/component/StackNavigator.js
@@ -2,12 +2,36 @@ import React, { useState} from 'react';
 import ChatBox from './ChatBox';
 import MessagesScreen from './messagesScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import Dash from './dash';
 import NearbyList from './NearbyList';
+import AddFriend from './addFriend';
+
 
 const Stack = createStackNavigator();
 
-const StackNavigator2 = ({auth}) => {
-    console.log(auth);
+const DashStackNavigator = ({auth, setAuth}) => {
+  return (
+    <Stack.Navigator initialRouteName="Dash">
+      <Stack.Screen
+          name="Dash Screen"
+          >
+          {(props) => <Dash {...props} auth={auth} setAuth={setAuth}
+          />}
+      </Stack.Screen>
+      <Stack.Screen
+          name="Example"
+          component={Example}
+          options={({route}) => ({
+             title: route.params.userName,
+             headerBackTitleVisible: false,
+           })}>
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
+}
+
+const ChatStackNavigator = ({auth}) => {
+    // console.log(auth);
     return (
       <Stack.Navigator initialRouteName="Dash">
         <Stack.Screen
@@ -28,15 +52,27 @@ const StackNavigator2 = ({auth}) => {
 }
 
 const NearbyStackNavigator = ({auth}) => {
-    console.log(auth)
-    return (
-        <Stack.Navigator initialRouteName="Nearby Screen">
-          <Stack.Screen
-              name="Nearby Screen">
-              {(props) => <NearbyList {...props} auth={auth} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      );
+  // console.log(auth)
+  return (
+      <Stack.Navigator initialRouteName="Nearby Screen">
+        <Stack.Screen
+            name="Nearby Screen">
+            {(props) => <NearbyList {...props} auth={auth} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    );
 }
 
-export { StackNavigator2, NearbyStackNavigator };
+const SettingsStackNavigator = ({auth}) => {
+  // console.log(auth)
+  return (
+      <Stack.Navigator initialRouteName="Settings Screen">
+        <Stack.Screen
+            name="Settings Screen">
+            {(props) => <AddFriend {...props} auth={auth} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    );
+}
+
+export { DashStackNavigator, ChatStackNavigator, NearbyStackNavigator, SettingsStackNavigator};
--- src/component/dash.js
+++ src/component/dash.js
@@ -1,80 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { View, TextInput, Text, Button, StyleSheet, StatusBar, SafeAreaView, FlatList, TouchableOpacity, Alert } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-// import geofire from 'geofire-common'
 navigator.geolocation = require('@react-native-community/geolocation');
-
-const geofire = require('geofire-common');
 
 const Dash = ({ setAuth, navigation, auth }) => {
   const [content, setContent] = useState('');
   const [field, setField] = useState('me');
-  const [location, setLocation] = useState({});
-  useEffect(() => {
-    if (location.latitude) {
-        saveUserLocation();
-    }
-  }, [location]);
-
+  const [location, setLocation] = useState();
   const handleAuth = () => {
     setAuth(false);
   }
 
   const findCoordinates = () => {
-    // console.log(auth);
     navigator.geolocation.getCurrentPosition(
       position => {
-        // const location = JSON.stringify(position);
-        // console.log(position['coords']);
-        
-        setLocation({longitude: position['coords']['longitude'], latitude: position['coords']['latitude']});
-
-        // console.log(location.latitude);
+        const location = JSON.stringify(position);
+        setLocation(location);
       },
       error => Alert.alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   };
 
-  // const calculateGeoHash = () => {
-  //   const hash = geofire.geohashForLocation([location.latitude, location.longitude]);
-  // }
-
-  const saveUserLocation = async() => {
-    const hash = geofire.geohashForLocation([location.latitude, location.longitude]);
-    const newLocation = {
-      geohash: hash,
-      latitude: location.latitude,
-      longitude: location.longitude
-    }
-    const users = await firestore()
-      .collection('users')
-      .doc(auth)
-      .update({'location': newLocation});
-  }
-
-  
-
-
-  // useEffect(() => {
-  // }, [])
-  
-  
-
+  useEffect(() => {
+  }, [])
   return (
     <>
     <Text> You're In!</Text>
     <Text> You are {auth}</Text>
     <Button
         title="Go to addFriend"
-        onPress={() => navigation.navigate('AddFriend')}
+        onPress={() => navigation.navigate('Add Friend')}
       />
     <Button title="Log Out" onPress={handleAuth}></Button>
 
     <TouchableOpacity onPress={findCoordinates}>
 					<Text style={styles.welcome}>Find My Coords?</Text>
-					<Text>Location: {location.longitude}</Text>
-          <Text>Location: {location.latitude}</Text>
+					<Text>Location: {location}</Text>
 				</TouchableOpacity>
     </>
   );
